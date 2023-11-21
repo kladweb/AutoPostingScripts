3f550c45757c2dc3a532410db12a4cb618a67d9d
Pavel Kladkevich <kladpav@gmail.com>
2023-11-21 22:14:03 +0300

    fix OK7

--- AutoRev_v3_SELF_F7_OK.js
+++ AutoRev_v3_SELF_F7_OK.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://ok.ru/profile/586178183434/groups
+// @match        https://ok.ru/profile/581088762021/groups
 // @run-at       context-menu
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ok.ru
 // @grant        none
@@ -12,11 +12,11 @@
 
 (function () {
   var linkPostsAll = [
-    "153761736441916",//Сравнение
-    "153761735589948",//cbilling
-    "154501128273980",//Szarovoz
-    "153761735917628",//VipLime
-    "153761736245308"//ILookTV
+    "156599140670389",//Сравнение
+    "156599117994933",//cbilling
+    "156189772602293",//Szarovoz
+    "151891231824821",//VipLime
+    "156614064196887"//ILookTV
   ];
   var linksPosts = [];
   var currentState = 0; //0 - все посты; 1 - первые три поста; 2 - последние два поста;
@@ -347,7 +347,7 @@
           console.log('checkIdAcc[1]: ', checkIdAcc[1]);
         }
       }
-      if ((checkIdAcc[0] && checkIdAcc[0] !== 586178183434) || (checkIdAcc[1] && checkIdAcc[1] !== 586178183434)) {
+      if ((checkIdAcc[0] && checkIdAcc[0] !== 581088762021) || (checkIdAcc[1] && checkIdAcc[1] !== 581088762021)) {
         console.log(`Найден чужой пост по заходу в группу: ${numberGroup}`);
         GroupsRepeat.push(numberGroup);
         refreshInterval = 180000;
