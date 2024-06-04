a0447159674bed7346875c546b22b2a2687df063
Pavel Kladkevich <kladpav@gmail.com>
2024-06-04 23:24:44 +0300

    change lists vk9, vk11

--- VK_0_Екатерина.js
+++ VK_0_Екатерина.js
@@ -16,22 +16,22 @@
     173498496,//14 https://vk.com/my1ottnet
     130237472,//24 https://vk.com/club130237472
     132944148,//27 https://vk.com/iptvlistok
-    // 18331470,//22	https://vk.com/marinaol
+    18331470,//22	https://vk.com/marinaol
     155500362,//26 https://vk.com/i_ptv
     114119485,//11 https://vk.com/aurahdclub
     140398176, //28 https://vk.com/oknotiviru
-    119600438,//23 https://vk.com/iptvzeus
+    // 119600438,//23 https://vk.com/iptvzeus
     106796170,//25 https://vk.com/club106796170
     52218536,//21	https://vk.com/club52218536
     // 99770042, //17 https://vk.com/club99770042
     // 65739319,//15 https://vk.com/iptvm3u
     // 138553819,//16 https://vk.com/club138553819
-    84120000, //18 https://vk.com/club84120000
-    176994995, //29 https://vk.com/tvlisty
-    167018774, //30 https://vk.com/club167018774
+    // 84120000, //18 https://vk.com/club84120000
+    // 176994995, //29 https://vk.com/tvlisty
+    // 167018774, //30 https://vk.com/club167018774
     171843329, //31 https://vk.com/ru_iptv
     // 120034509, //32 https://vk.com/club120034509
-    131638330, //33 https://vk.com/galaktik_iptv
+    // 131638330, //33 https://vk.com/galaktik_iptv
   ]
 
   var numberGroups = groupsAll.length;
