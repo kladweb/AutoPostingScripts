e5e408a6449b3835aad128192e1068a95a949a6b
Pavel Kladkevich <kladpav@gmail.com>
2024-11-01 08:47:09 +0300

    add group iptvstreamshub

--- VK_11_Тис.js
+++ VK_11_Тис.js
@@ -12,6 +12,7 @@
   var _apiName2 = 'VK11P3';
 
   var groupsAll = [
+    // 198518322, //https://vk.com/iptvstreamshub
     140398176, //28 https://vk.com/oknotiviru
     14875387,//Bel https://vk.com/club14875387
     18331470,//22	https://vk.com/marinaol
@@ -25,7 +26,7 @@
     99770042, //17 https://vk.com/club99770042
     52218536,//21	https://vk.com/club52218536
     65739319,//15 https://vk.com/iptvm3u
-    138553819,//16 https://vk.com/club138553819
+    // 138553819,//16 https://vk.com/club138553819
     84120000, //18 https://vk.com/club84120000
     176994995, //29 https://vk.com/tvlisty
     167018774, //30 https://vk.com/club167018774
@@ -203,7 +204,7 @@
   var action5 = function () {
     console.log('action5');
     setTimeout(() => {
-      var buttonNews = document.querySelector('.LeftMenuItem-module__item--XMcN9[href="/feed"]');
+      var buttonNews = document.querySelector('a[href="/feed"]');
       if (buttonNews) {
         buttonNews.click();
         setTimeout(() => {
