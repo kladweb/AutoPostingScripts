3e65e6eb5d76d8dec10d67cefe5b413c21993c60
Pavel Kladkevich <kladpav@gmail.com>
2024-12-14 23:06:26 +0300

    fix: correct submit for VK0 and VK11

--- VK_submit_mod01/VK2_Татьяна_Андреева_mod01.js
+++ VK_submit_mod01/VK2_Татьяна_Андреева_mod01.js
@@ -13,21 +13,21 @@
   // postObjectP2 - ILookTV
 
   const groupsAll = [
-    14875387,//Bel https://vk.com/club14875387
-    198518322, //https://vk.com/iptvstreamshub
-    173498496,//14 https://vk.com/my1ottnet
-    130237472,//24 https://vk.com/club130237472
-    18331470,//22	https://vk.com/marinaol
-    132944148,//27 https://vk.com/iptvlistok
-    155500362,//26 https://vk.com/i_ptv
-    114119485,//11 https://vk.com/aurahdclub
+    // 14875387,//Bel https://vk.com/club14875387
+    // 198518322, //https://vk.com/iptvstreamshub
+    // 173498496,//14 https://vk.com/my1ottnet
+    // 130237472,//24 https://vk.com/club130237472
+    // 18331470,//22	https://vk.com/marinaol
+    // 132944148,//27 https://vk.com/iptvlistok
+    // 155500362,//26 https://vk.com/i_ptv
+    // 114119485,//11 https://vk.com/aurahdclub
     140398176, //28 https://vk.com/oknotiviru
     106796170,//25 https://vk.com/club106796170
     119600438,//23 https://vk.com/iptvzeus
     99770042, //17 https://vk.com/club99770042
     52218536,//21 https://vk.com/club52218536
     65739319,//15 https://vk.com/iptvm3u
-    138553819,//16 https://vk.com/club138553819
+    // 138553819,//16 https://vk.com/club138553819
     84120000, //18 https://vk.com/club84120000
     176994995, //29 https://vk.com/tvlisty
     167018774, //30 https://vk.com/club167018774
@@ -254,7 +254,8 @@
   };
 
   const postObjects = [
-    postObjectP1, //VipDrive
+    postObjectP2, //ILookTV
+    // postObjectP1, //VipDrive
     // postObjectP2, //ILookTV
   ];
 
@@ -293,6 +294,7 @@
     console.log('action3');
     setTimeout(() => {
       var buttonFar = document.querySelector('[data-testid="posting_base_screen_next"]');
+      console.log(buttonFar);
       if (buttonFar) {
         console.log('action3 buttonFar');
         buttonFar.click();
@@ -420,6 +422,7 @@
     console.log('action02');
     setTimeout(() => {
       var linkGroup = document.querySelector(`#gl_groups${groupsAll[currentNumberGr]} .group_row_title`);
+      // var linkGroup = document.querySelector(`#gl_groups${groupsAll[currentNumberGr]} .AvatarRich`);
       console.log('linkGroup: ', linkGroup);
       if (linkGroup) {
         linkGroup.click();
