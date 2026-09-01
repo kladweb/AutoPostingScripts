be19ffe92c2c770aea4983704c3a350edb740f6b
Pavel Kladkevich <kladpav@gmail.com>
2026-09-01 17:46:51 +0300

    feat: add origins

---
+++ origins/main_OK_Veronika.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         OK_Veronika_v2
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @author       Paha
+// @match        https://ok.ru/profile/575084661978/groups
+// @grant        none
+// @run-at       document-end
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ok.ru
+// @require      https://raw.githubusercontent.com/kladweb/autoposting-scripts/refs/heads/main/build/VK0_fin.js
+// ==/UserScript==
