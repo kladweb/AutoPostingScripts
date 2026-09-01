148dfa25753d7029fa31ccf69bd459f112a873ac
Pavel Kladkevich <kladpav@gmail.com>
2026-09-01 17:42:06 +0300

    feat: add husky

---
+++ src/OK_Tis.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         OK_Tis_v2
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       KP
+// @match        https://ok.ru/profile/586178183434/groups
+// @run-at       document-end
+// @updateURL    https://raw.githubusercontent.com/kladweb/autoposting-scripts/refs/heads/main/build/VK0_fin.js
+// @downloadURL  https://raw.githubusercontent.com/kladweb/autoposting-scripts/refs/heads/main/build/VK0_fin.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const userId = "586178183434";
+
+  // === TEMPLATE START ===
+  // insert here
+  // === TEMPLATE END ===
+
+})();
---
+++ src/OK_Veronika.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         OK_Veronika_v2
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       KP
+// @match        https://ok.ru/profile/575084661978/groups
+// @run-at       document-end
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ok.ru
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const userId = "575084661978";
+
+  // === TEMPLATE START ===
+  // insert here
+  // === TEMPLATE END ===
+
+
+})();
